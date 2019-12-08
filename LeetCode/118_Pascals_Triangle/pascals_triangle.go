8fdd88cc81ae55da8a857a9249c674d9c79e2882
flada-auxv <aseknock@gmail.com>
2019-12-08 23:43:58 +0900

    できた

--- LeetCode/118_Pascals_Triangle/pascals_triangle.go
+++ LeetCode/118_Pascals_Triangle/pascals_triangle.go
@@ -8,8 +8,36 @@ func Generate(numRows int) [][]int {
 func generate(numRows int) [][]int {
 	var result [][]int
 
-	for i := 0; i < numRows; i++ {
-		result = append(result, []int{1})
+	if numRows == 0 {
+		return [][]int{}
+	}
+
+	for level := 0; level < numRows; level++ {
+		if level == 0 {
+			result = append(result, []int{1})
+			continue
+		}
+
+		var row []int
+		for col := 0; col < level+1; col++ {
+			num := 0
+
+			if col-1 < 0 {
+				// fmt.Printf("[left edge]level: %v, col: %v\n", level, col)
+				num = num + result[level-1][col]
+			} else if col == level {
+				// fmt.Printf("[right edge]level: %v, col: %v\n", level, col)
+				num = num + result[level-1][col-1]
+			} else {
+				// fmt.Printf("[center]level: %v, col: %v\n", level, col)
+				num = num + result[level-1][col]
+				num = num + result[level-1][col-1]
+			}
+
+			row = append(row, num)
+		}
+
+		result = append(result, row)
 	}
 
 	return result
--- LeetCode/118_Pascals_Triangle/pascals_triangle_test.go
+++ LeetCode/118_Pascals_Triangle/pascals_triangle_test.go
@@ -5,13 +5,31 @@ import (
 	"testing"
 )
 
-func TestGenerate(t *testing.T) {
-	if len(Generate(1)) != 1 {
-		t.Errorf("error")
+func TestGenerateCase1(t *testing.T) {
+	actual := Generate(0)
+	expected := [][]int{}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected: %v, actual: %v", expected, actual)
 	}
+}
 
+func TestGenerateCase2(t *testing.T) {
 	actual := Generate(1)
 	expected := [][]int{[]int{1}}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestGenerateCase3(t *testing.T) {
+	actual := Generate(5)
+	expected := [][]int{
+		[]int{1},
+		[]int{1, 1},
+		[]int{1, 2, 1},
+		[]int{1, 3, 3, 1},
+		[]int{1, 4, 6, 4, 1},
+	}
 	if !reflect.DeepEqual(actual, expected) {
 		t.Fatalf("expected: %v, actual: %v", expected, actual)
 	}
