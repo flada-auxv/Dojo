e935ac72c43cf53dee414f4c9f46c9220202bcf8
flada-auxv <aseknock@gmail.com>
2020-01-29 23:46:02 +0900

    split_by_power_of_two

--- misc/merkle_chain/merkle_tree.rb
+++ misc/merkle_chain/merkle_tree.rb
@@ -2,10 +2,20 @@
 
 module MT
   class MerkleTree
-    attr_accessor :elements
+    attr_reader :elements
 
     def initialize(elements = [])
+      raise ArgumentError if elements.length < 2
+
       @elements = elements
+
+      build_tree
+    end
+
+    def root_hash
+      return @root.leaf_hash if @elements.length == 1
+
+      @root.inner_hash
     end
 
     def proof(target_value:)
@@ -16,6 +26,24 @@ module MT
       return true if target_value == 3
 
       false
+    end
+
+    def build_tree
+      left, right = split_by_power_of_two
+      @root = Node.new
+      @root.add_left(left)
+      @root.add_right(right)
+      @root.traverse_down
+    end
+
+    # the largest powers of two less than the number of elements
+    def split_index
+      binary_length = (@elements.length - 1).to_s(2).length
+      @split_index ||= 2**(binary_length - 1) - 1
+    end
+
+    def split_by_power_of_two
+      [@elements[0..split_index], @elements[split_index+1..]]
     end
   end
 
@@ -29,11 +57,22 @@ module MT
       @right = right
     end
 
-    def hash
-      OpenSSL::Digest::SHA256(@value)
+    def leaf_hash
+      # OpenSSL::Digest::SHA256.hexdigest(@value.to_s)
+      @value
+    end
+
+    def inner_hash
+      return nil unless @left && @right
+
+      "#{@left}+#{@right}"
     end
 
     def root?
+      true
+    end
+
+    def leaf?
       true
     end
 
@@ -46,6 +85,14 @@ module MT
       else
         raise ArgumentError
       end
+    end
+
+    def add_left(node)
+      add(node, to: :left)
+    end
+
+    def add_right(node)
+      add(node, to: :right)
     end
   end
 end
--- misc/merkle_chain/merkle_tree_test.rb
+++ misc/merkle_chain/merkle_tree_test.rb
@@ -5,12 +5,15 @@ require_relative './merkle_tree'
 
 class TestMerkleTree < Minitest::Test
   def setup
-    elements = [1, 2, 3, 4, 5]
+    elements = %w[1 2 3 4 5]
     @merkle_tree = MT::MerkleTree.new(elements)
   end
 
   def test_initialize
-    assert_equal([1, 2, 3, 4, 5], MT::MerkleTree.new([1, 2, 3, 4, 5]).elements)
+    assert_equal(%w[1 2 3 4 5], MT::MerkleTree.new(%w[1 2 3 4 5]).elements)
+
+    assert_raises(ArgumentError) { MT::MerkleTree.new([]) }
+    assert_raises(ArgumentError) { MT::MerkleTree.new([1]) }
   end
 
   def test_verify
@@ -28,6 +31,34 @@ class TestMerkleTree < Minitest::Test
   def test_proof
     expected = ['4', '1+2', '5']
     assert_equal(expected, @merkle_tree.proof(target_value: 3))
+  end
+
+  def test_root_hash
+    assert_equal('1', @merkle_tree.root_hash)
+  end
+
+  def test_split_index
+    assert_equal(0, MT::MerkleTree.new(%w[1 2]).split_index)
+    assert_equal(1, MT::MerkleTree.new(%w[1 2 3]).split_index)
+    assert_equal(1, MT::MerkleTree.new(%w[1 2 3 4]).split_index)
+    assert_equal(3, MT::MerkleTree.new(%w[1 2 3 4 5]).split_index)
+    assert_equal(7, MT::MerkleTree.new(%w[1 2 3 4 5 6 7 8 9]).split_index)
+  end
+
+  def test_split_by_power_of_two
+    assert_equal(
+      [%w[1], %w[2]],
+      MT::MerkleTree.new(%w[1 2]).split_by_power_of_two
+    )
+    assert_equal(
+      [%w[1 2], %w[3]],
+      MT::MerkleTree.new(%w[1 2 3]).split_by_power_of_two
+    )
+
+    assert_equal(
+      [%w[1 2 3 4 5 6 7 8], %w[9]],
+      MT::MerkleTree.new(%w[1 2 3 4 5 6 7 8 9]).split_by_power_of_two
+    )
   end
 end
 
